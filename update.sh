62f324bcca29a11218bf82649897be83e160f03b
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-20 21:05:20 +1100

    updated dependancies

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 cd ../subtle-crypto-window/
+npm run-script build
 git add index.js @.js
 git commit -m "auto update"
 git push
 cd ../qr-subtle-ws/
+git add index.js @.js
 ./update_git_repos.sh push
